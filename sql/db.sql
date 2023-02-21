d5c5ad9eaf8a32be91a198939c126db24aff6249
InnsbruckTaos <96445516+jkosixtyfour@users.noreply.github.com>
2023-02-21 18:55:52 -0500

    update db template

--- sql/db.sql
+++ sql/db.sql
@@ -1,4 +1,17 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 22, 2023 at 12:54 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+--
+-- revista official database import file (accurate as of 21 february 2023)
+--
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -7,10 +20,33 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `revista`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `announcements`
+--
+-- Creation: Feb 20, 2023 at 11:00 PM
+-- Last update: Feb 21, 2023 at 11:40 PM
+--
+
+CREATE TABLE `announcements` (
+  `id` int(11) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `comments`
+--
+-- Creation: Feb 17, 2023 at 07:01 PM
+-- Last update: Feb 20, 2023 at 10:09 PM
 --
 
 CREATE TABLE `comments` (
@@ -19,29 +55,36 @@ CREATE TABLE `comments` (
   `author` varchar(255) NOT NULL,
   `comment` varchar(255) NOT NULL,
   `date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
+-- Creation: Feb 20, 2023 at 11:25 PM
+-- Last update: Feb 20, 2023 at 11:25 PM
+--
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `date` datetime NOT NULL,
-  `subscribers` int(11) NOT NULL DEFAULT '0',
+  `subscribers` int(11) NOT NULL DEFAULT 0,
   `username` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL DEFAULT 'No description.',
+  `description` varchar(255) NOT NULL DEFAULT 'v',
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `pfp` varchar(255) NOT NULL DEFAULT 'default.png'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `pfp` varchar(255) NOT NULL DEFAULT 'acc.jpg',
+  `is_admin` int(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `videos`
+--
+-- Creation: Feb 17, 2023 at 07:01 PM
+-- Last update: Feb 21, 2023 at 03:11 AM
 --
 
 CREATE TABLE `videos` (
@@ -49,31 +92,23 @@ CREATE TABLE `videos` (
   `vid` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL DEFAULT '',
   `author` varchar(255) NOT NULL,
-  `likes` int(11) NOT NULL DEFAULT '0',
+  `likes` int(11) NOT NULL DEFAULT 0,
   `date` datetime NOT NULL,
-  `views` int(11) NOT NULL DEFAULT '0',
+  `views` int(11) NOT NULL DEFAULT 0,
   `filename` varchar(255) NOT NULL,
-  `featured` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `announcements`
---
-
-CREATE TABLE `announcements` (
-  `id` int(11) NOT NULL,
-  `author` varchar(255) NOT NULL,
-  `content` varchar(255) NOT NULL,
-  `date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+  `featured` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `announcements`
+--
+ALTER TABLE `announcements`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
 
 --
 -- Indexes for table `comments`
@@ -98,15 +133,24 @@ ALTER TABLE `videos`
 --
 
 --
+-- AUTO_INCREMENT for table `announcements`
+--
+ALTER TABLE `announcements`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
