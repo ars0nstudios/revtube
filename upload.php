00779181c75c2dfc4a49d20d9c16acf1312e6c8e
InnsbruckTaos <risk61358@gmail.com>
2023-02-24 22:03:04 -0500

    :(

--- upload.php
+++ upload.php
@@ -72,9 +72,9 @@
                 curl_setopt( $ch, CURLOPT_HEADER, 0);
                 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
                 $response = curl_exec( $ch );
-                echo('<script>
-              window.location.href = "index.php?msg=Your video has been uploaded!";
-              </script>');
+               // echo('<script>
+              //window.location.href = "index.php?msg=Your video has been uploaded!";
+              //</script>');
             } else {
                 echo "error!!!!!!!!!!!!!!!!!! error code: ";
                 echo $_FILES["fileToUpload"]["error"];
