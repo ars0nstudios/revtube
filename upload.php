ebda06b8cde009bdf939ae504c329092a32f223a
InnsbruckTaos <risk61358@gmail.com>
2023-02-24 22:25:10 -0500

    fuck

--- upload.php
+++ upload.php
@@ -48,10 +48,7 @@
             ";
         } else {
             if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                $gruh = $_FILES["fileToUpload"]["tmp_name"];
-                $source = "./content/tmp/$gruh";
-                $src = trim($source);
-                rename("$src", "content/videos/$v_id.mp4");
+                rename("$target_file", "content/videos/$v_id.mp4");
                 $video = $_POST['videotitle'];
                 $user = $_SESSION['profileuser3'];
               //  $v_id = randstr(11);
