c3dd61812f7e30af267d11cb7f71e4597a8946ac
InnsbruckTaos <96445516+jkosixtyfour@users.noreply.github.com>
2023-03-06 11:16:31 -0500

    fix deadlink yo

---
+++ community.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <?php include './assets/mod/meta.php';?>      
+</head>
+
+  <body>
+<?php include './assets/mod/db.php';?>
+<?php include './assets/mod/header.php';?>
+    <div class="container">
+ <div class="content">
+        <div class="page-header">
+        <?php include './assets/mod/alert.php';?>
+          <h1>Community</h1>
+        </div>
+        <div class="row">
+          <div class="span10">
+            <h2>Discord</h2>
+            <p>We have a Discord server.</p>page will be finish later, the developer is in school right now and discord is blocked
+          </div>
+          <div class="span4">
+<?php include './assets/mod/whatsnew.php'; ?>
+      </div>
+
+      <?php include './assets/mod/footer.php'; ?>
+    </div> <!-- /container -->
+
+  </body>
+</html>
